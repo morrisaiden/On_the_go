83936799651fdd582cca4e1f66251425943750b8
morrisaiden <mwaimorris1997@gmail.com>
2024-10-22 09:38:18 +0300

    masterapp app

--- masterapp/admin.py
+++ masterapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Product, CartItem
+from .models import Product, CartItem, Footwear
 
-# Register your models here.
 admin.site.register(Product)
 admin.site.register(CartItem)
+admin.site.register(Footwear)
+
--- masterapp/urls.py
+++ masterapp/urls.py
@@ -1,18 +1,11 @@
+from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-
-from On_the_go import settings
 from . import views
 
-app_name = 'masterapp' #name space
+app_name = 'masterapp'
 
 urlpatterns = [
-    # path('', views.shop_home, name='shop_home'),
-    # path('items', views.item_list, name='item_list'),
-    # path('masterapp/', views.cart_detail, name='cart_detail'),
-    # path('add_to_cart/<str:item_type>/<int:item_id>/<int:quantity>/', views.add_to_cart, name='add_to_cart'),
-    # path('add_to_cart/<str:item_type>/<int:item_id>/<int:quantity>/', views.add_to_cart, name='add_to_cart'),
-
     path('', views.product_list, name='product_list'),
     path('cart/', views.view_cart, name='view_cart'),
     path('add/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
@@ -21,5 +14,5 @@ urlpatterns = [
     path('details/', views.details, name='details'),
     path('contact/', views.contact, name='contact'),
     path('cart/', views.cart, name='cart'),
-    # path('Shoes/', views.Footwear_list, name='Footwear_list'),
+    path('Shoes/', views.Footwear_list, name='Footwear_list'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
