bc0cd41a366e8d0c0ad374f596754c71925ae295
morrisaiden <mwaimorris1997@gmail.com>
2024-10-23 14:20:35 +0300

    login && signup logic

--- masterapp/views.py
+++ masterapp/views.py
@@ -4,6 +4,11 @@ from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from .models import Product, CartItem, Footwear
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout, login, authenticate
+
 
 
 def product_list(request):
@@ -93,3 +98,56 @@ def empty_cart(request):
         request.session.modified = True
 
     return redirect('your_desired_redirect_url')
+
+
+def handlelogin(request):
+    if request.method == "POST":
+        uname = request.POST.get("username")
+        pass1 = request.POST.get("pass1")
+        myuser = authenticate(username=uname, password=pass1)
+        if myuser is not None:
+            login(request, myuser)
+            messages.success(request, "Login Success")
+            return redirect('/')
+        else:
+            messages.error(request, "Invalid Credentails")
+            return redirect('/login')
+    return render(request, 'login.html')
+
+
+def handlesignup(request):
+    if request.method == "POST":
+        uname = request.POST.get("username")
+        email = request.POST.get("email")
+        password = request.POST.get("pass1")
+        confirmpassword = request.POST.get("pass2")
+        # print(uname,email,password,confirmpassword)
+        if password != confirmpassword:
+            messages.warning(request, "Password is Incorrect")
+            return redirect('/signup')
+
+        try:
+            if User.objects.get(username=uname):
+                messages.info(request, "UserName Is Taken")
+                return redirect('/signup')
+        except:
+            pass
+        try:
+            if User.objects.get(email=email):
+                messages.info(request, "Email Is Taken")
+                return redirect('/signup')
+        except:
+            pass
+
+        myuser = User.objects.create_user(uname, email, password)
+        myuser.save()
+        messages.success(request, "Signup Success Please login!")
+        return redirect('/login')
+
+    return render(request, 'signup.html')
+
+
+def handlelogout(request):
+    logout(request)
+    messages.info(request, "Logout Success")
+    return redirect('/login')
